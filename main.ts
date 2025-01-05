5472493d98ba49fa1d9dcd81c023736c7335a059
RemiMak <remimakinwa@outlook.com>
2025-01-05 13:40:43 +0100

    Syntax is checked and errors are shown to the user

--- main.ts
+++ main.ts
@@ -18,10 +18,27 @@ export default class MyPlugin extends Plugin {
 
 		this.registerMarkdownCodeBlockProcessor('glsl', (source, el, ctx) => {
 			const glsl_canvas = el.createEl('canvas');
-			glsl_canvas.style.width = '100%';
-			const glsl_sandbox = new GlslCanvas(glsl_canvas);
-			const fragmentShader = source;
-			glsl_sandbox.load(fragmentShader);
+			const glsl_context = glsl_canvas.getContext('webgl');
+			if (!glsl_context) {
+				console.log('Error creating WebGL context');
+				return
+			}
+
+			const error_message = this.checkShaderSyntax(glsl_context, source);
+			
+			// if there is a syntax error, display it to the user
+			if (error_message) { 
+				el.removeChild(glsl_canvas);
+				this.addSyntaxErrorElements(el, error_message, source);
+			}
+			
+			// if there is no syntax error, display the shader to the user
+			else {
+				glsl_canvas.style.width = '100%';
+				const glsl_sandbox = new GlslCanvas(glsl_canvas);
+				const fragmentShader = source;
+				glsl_sandbox.load(fragmentShader);
+			}			
 		});
 
 
@@ -87,6 +104,69 @@ export default class MyPlugin extends Plugin {
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	}
+
+	checkShaderSyntax(gl_context: WebGLRenderingContext, shader_code: string) : string | null{
+		const shader = gl_context.createShader(gl_context.FRAGMENT_SHADER);
+
+		if (!shader) { return 'Error creating shader';}
+
+		gl_context.shaderSource(shader, shader_code);
+		gl_context.compileShader(shader);
+
+		const was_succesfully_compiled = gl_context.getShaderParameter(
+				shader, gl_context.COMPILE_STATUS
+		);
+
+		if (was_succesfully_compiled) {
+			gl_context.deleteShader(shader);
+			return null;
+		} else {
+			const error_message = gl_context.getShaderInfoLog(shader);
+			gl_context.deleteShader(shader);
+			return error_message;
+		}
+	}
+
+	addSyntaxErrorElements(el: HTMLElement, error_message: string, source: string) {
+		const lines = source.split('\n');
+		const error_line = parseInt(error_message.split(':')[2]);
+		const error_message_el = el.createEl('div');
+		error_message_el.setText(error_message);
+		error_message_el.style.color = 'red';
+		error_message_el.style.fontWeight = 'bold';
+		error_message_el.style.marginBottom = '10px';
+		error_message_el.style.whiteSpace = 'pre-wrap';
+		error_message_el.style.overflow = 'auto';
+		error_message_el.style.width = '100%';
+		error_message_el.style.backgroundColor = 'rgba(255, 0, 0, 0.1)';
+		error_message_el.style.padding = '10px';
+		el.appendChild(error_message_el);
+
+		const code_el = el.createEl('pre');
+		code_el.style.whiteSpace = 'pre-wrap';
+		code_el.style.overflow = 'auto';
+		code_el.style.width = '100%';
+		code_el.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+		code_el.style.padding = '10px';
+		el.appendChild(code_el);
+		
+		for (let i = 0; i < lines.length; i++) {
+			const line_el = code_el.createEl('div');
+			line_el.style.font = '12px monospace';
+			
+			if (lines[i].length > 0) {
+				line_el.setText(lines[i]);
+			}
+
+			else {
+				line_el.setText(' ');
+			}
+
+			if (i === error_line - 1) {
+				line_el.style.backgroundColor = 'rgb(255, 0, 0)';
+			}
+		}
 	}
 
 	onunload() {
