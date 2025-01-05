52aedbd1642e651f515b4af15958651be5ff6502
RemiMak <remimakinwa@outlook.com>
2025-01-05 14:53:34 +0100

    user can override default width percent and aspect ratio
    For if you have shaders which look better if not a square

--- main.ts
+++ main.ts
@@ -34,11 +34,23 @@ export default class MyPlugin extends Plugin {
 			
 			// if there is no syntax error, display the shader to the user
 			else {
+				// default width and aspect ratio
+				glsl_canvas.style.width = '50%';
+				glsl_canvas.style.aspectRatio = '1';
+				
+				// user defined width and aspect ratio
+				const {width, aspect_ratio} = this.getCanvasSizeParameters(source);
+				if (width) {
+					glsl_canvas.style.width = width;
+				}
+
+				if (aspect_ratio) {
+					glsl_canvas.style.aspectRatio = aspect_ratio;
+				}
+
 				glsl_canvas.style.margin = '0 auto';
 				glsl_canvas.style.display = 'block';
-				glsl_canvas.style.width = '50%';
 				glsl_canvas.style.height = 'auto';
-				glsl_canvas.style.aspectRatio = '1/1';
 				
 				const glsl_sandbox = new GlslCanvas(glsl_canvas);
 				const fragmentShader = source;
@@ -47,67 +59,6 @@ export default class MyPlugin extends Plugin {
 		});
 
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a REMI!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	checkShaderSyntax(gl_context: WebGLRenderingContext, shader_code: string) : string | null{
@@ -130,6 +81,17 @@ export default class MyPlugin extends Plugin {
 			gl_context.deleteShader(shader);
 			return error_message;
 		}
+	}
+
+	getCanvasSizeParameters(shader_code: string) : {width: string | null, aspect_ratio: string | null} {
+		const lines = shader_code.split('\n');
+		const width_line = lines.find(line => line.includes('glsl_canvas_width'));
+		const aspectRatio_line = lines.find(line => line.includes('glsl_canvas_aspect_ratio'));
+
+		const width = width_line ? (width_line.split('=')[1]) : null;
+		const aspect_ratio = aspectRatio_line ? (aspectRatio_line.split('=')[1]) : null;
+
+		return {width, aspect_ratio};
 	}
 
 	addSyntaxErrorElements(el: HTMLElement, error_message: string, source: string) {
